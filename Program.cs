7a2ad493d024bcd987097eef2bb3864f29f4d43a
Geoffrey Dolbear <339470494@GoTVDSB.ca>
2024-02-28 13:17:27 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_5___If_Statments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Automatic Teller Machine, would you like");
+        }
+    }
+}
